e299fe61c7f19eef87b1d8abe43a6a5419ec4c02
pratimkanrar <root@kali>
2020-10-21 21:35:40 +0530

    touch device

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import {render} from 'react-dom';
 import {App} from './App';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
+
+
+const is_touch_enabled = () => {
+    return ( 'ontouchstart' in window ) || ( navigator.maxTouchPoints > 0 ) || ( navigator.msMaxTouchPoints > 0 );
+}
 
 render(
-    <DndProvider backend={HTML5Backend}>
-      <App />
+    <DndProvider backend={is_touch_enabled() ? TouchBackend : HTML5Backend}>
+      <App/>
     </DndProvider>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
