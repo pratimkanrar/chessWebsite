647612cb5c20fcc68a15d51fb0d59d1b9c6f0280
pratimkanrar <root@kali>
2020-10-21 21:52:36 +0530

    Updates

---
+++ precache-manifest.5a7003791e0b22df1b6aa02782faf326.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a406039e9e7c3d7a95c6ae2b8422a7d",
+    "url": "/chessWebsite/index.html"
+  },
+  {
+    "revision": "ee8b7bd5697deba6f356",
+    "url": "/chessWebsite/static/css/main.e64c6c93.chunk.css"
+  },
+  {
+    "revision": "e54507da7ae61e027d80",
+    "url": "/chessWebsite/static/js/2.714ab4b1.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/chessWebsite/static/js/2.714ab4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8b7bd5697deba6f356",
+    "url": "/chessWebsite/static/js/main.826f6b32.chunk.js"
+  },
+  {
+    "revision": "1f643522459e6fc17eb3",
+    "url": "/chessWebsite/static/js/runtime-main.fa239b25.js"
+  },
+  {
+    "revision": "3382a95341b72c229183dcc0f1168176",
+    "url": "/chessWebsite/static/media/q_w.3382a953.png"
+  }
+]);
